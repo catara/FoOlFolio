2da8962de5f8ece8c904848296a5d0b9e577e4d0
Woxxy <woxxy@foolrulez.org>
2011-11-30 23:39:48 +0100

    added some content

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,37 @@
-HERE
+<div class="ninesixzero">
+
+	<article class="home">
+		<header>
+			<h1>What if we recoded old concepts to make them awesome?</h1>
+		</header>
+		<h2>
+			FoOlRulez does that.<br/>
+			There's plenty of rusty and still popular 
+			interfaces that aren't going to go out of order just yet. These concepts are
+			as old as HTML itself, and we want to give them a lifting.
+		</h2>
+
+		<aside>
+			<header>
+				<h3>Our mission:</h3>
+			</header>
+			<p>
+				Recognize where the paradigm must change, and make the users go<br/>
+				<strong>«Woah, I never thought it could be BUZZWORD!»</strong>
+			</p>
+		</aside>
+		
+		<article class="showcase">
+			<header>
+				<h2>So, how about you check out what did we upgrade?</h2>
+			</header>
+			
+			<section>
+				<header>
+					<a href="http://wiki.foolslide.org"><h3>FoOlSlide</h3></a>
+					<a href="http://wiki.foolslide.org"><img src="<?php echo site_url()?>assets/theme/images/showcase_foolslide.png" alt="FoOlSlide" width="320"/></a>
+				</header>
+			</section>
+		</article>
+	</article>
+</div>
